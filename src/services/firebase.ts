5b53e34131202aea64b25fd063609f3098825ef9
Miguel Ângelo <miguelcg958@gmail.com>
2022-10-08 13:56:54 -0300

    chore: add database env to example env file

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,11 +5,11 @@ import { getDatabase } from 'firebase/database';
 const firebaseConfig: FirebaseOptions = {
   apiKey: import.meta.env.VITE_APP_API_KEY,
   authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_APP_DATABASE_URL,
   projectId: import.meta.env.VITE_APP_PROJECT_ID,
   storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_APP_ID,
-  databaseURL: import.meta.env.VITE_APP_DATABASE_URL,
 };
 
 export const app = initializeApp(firebaseConfig);
