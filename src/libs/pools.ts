fba7a9875eded01397e5a5ab656727e862846f90
Miguel Ângelo <miguelcg958@gmail.com>
2022-10-09 20:44:06 -0300

    feat: add get user pools method

--- src/libs/pools.ts
+++ src/libs/pools.ts
@@ -1,8 +1,19 @@
-import { get, ref } from 'firebase/database';
+import { equalTo, get, orderByChild, query, ref } from 'firebase/database';
 
 import { database } from '../services/firebase';
-import { Pool, RawPool } from '../typings';
+import type { Pool, RawPool } from '../typings';
 import { mapRawSongs } from './songs';
+
+function mapRawPool(rawPool: RawPool & { id: string }): Pool {
+  return {
+    id: rawPool.id,
+    title: rawPool.title,
+    songs: Object.entries(rawPool.songs || {}).map(([songId, rawSong]) =>
+      mapRawSongs(songId, rawSong)
+    ),
+    owner: rawPool.owner,
+  };
+}
 
 async function getPool(id: string): Promise<Pool | null> {
   const poolRef = ref(database, `pools/${id}`);
@@ -13,16 +24,22 @@ async function getPool(id: string): Promise<Pool | null> {
 
   const rawPool: RawPool = snapshot.val();
 
-  const pool: Pool = {
-    id: snapshot.key!,
-    title: rawPool.title,
-    songs: Object.entries(rawPool.songs || {}).map(([songId, rawSong]) =>
-      mapRawSongs(songId, rawSong)
-    ),
-    owner: rawPool.owner,
-  };
+  const pool: Pool = mapRawPool({ id, ...rawPool });
 
   return pool;
 }
 
-export { getPool };
+async function getUserPools(userId: string): Promise<Pool[]> {
+  const poolsRef = ref(database, 'pools');
+  const poolsQuery = query(poolsRef, orderByChild('owner/id'), equalTo(userId));
+
+  const snapshot = await get(poolsQuery);
+
+  if (!snapshot.exists()) return [];
+
+  return Object.entries(snapshot.val() as { [key: string]: RawPool }).map(([key, rawPool]) =>
+    mapRawPool({ id: key, ...rawPool })
+  );
+}
+
+export { getPool, getUserPools };
